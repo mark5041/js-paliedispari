6ee35e1eb74515889bc81aeff3a4dcc3088b4ebb
mark5041 <mark.beloso5041@gmail.com>
2021-11-29 15:22:52 +0100

    palindroma completo

--- Palidroma/js/script.js
+++ Palidroma/js/script.js
@@ -2,4 +2,36 @@
 // Creare una funzione per capire se la parola inserita è palindroma
 // (prima senza funzione e poi con funzione)
 
+let parola;
+
+function controlloParola(string) 
+{
+    let x = string.length - 1;
+    let i = 0;
+    let value = " non è una parola palindroma";
+    while(string[i] == string[x] && value == " non è una parola palindroma")
+    {
+        if(i + 1 == x - 1 || x - 1 == i)
+        {
+            value = " è una parola palindroma";
+        }
+        else
+        {
+            i++;
+            x--;
+        }
+    }
+    return value;
+    
+}
+
+do
+{
+    parola = prompt("inserisci una parola");
+}
+while(parola == null)
+
+alert(parola + controlloParola(parola));
+
+
 
