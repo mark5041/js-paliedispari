1d421ec1dc0479f417753cac12a4977a83f4e31c
mark5041 <mark.beloso5041@gmail.com>
2021-11-29 15:39:20 +0100

    Pari e Dispari completo

--- Pari e Dispari/js/script.js
+++ Pari e Dispari/js/script.js
@@ -7,3 +7,38 @@
 // In un secondo momento creiamo delle funzioni per i numeri random e per il check pari dispari
 
 
+let valore;
+let num1;
+
+function PariDispari(string, number1)
+{
+    let value = "dispari";
+    let number2 = Math.floor(Math.random() * 6) + 1;
+    if((number1 + number2) % 2 == 0)
+    {
+        value = "pari";
+    }
+
+    if(string == value)
+    {
+        return "hai vinto";
+    }
+    else
+    {
+        return "hai perso";
+    }
+}
+
+do
+{
+    valore = prompt("pari o dispari");
+}
+while(valore == null || (valore != "pari" && valore != "dispari"))
+
+do
+{
+    num1 = parseFloat(prompt("inserisci un numero compreso tra 1 e 5"));
+}
+while(isNaN(num1))
+
+alert(PariDispari(valore, num1));
